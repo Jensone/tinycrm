7563990f7538f9c73844c12cd663fb8db59e5fa9
Jensone <hello@jensone.com>
2024-02-05 13:14:02 +0100

    🔧 corriger(PaymentController.php) : importer les classes manquantes et renommer le service StripeService ✨ fonctionnalité(PaymentController.php) : ajouter des routes pour les pages de succès et d'annulation du paiement 🔧 corriger(PaymentController.php) : utiliser la clé API secrète de Stripe à partir des paramètres de l'application ✨ fonctionnalité(PaymentController.php) : effectuer un paiement en utilisant le service StripeService et les informations de l'offre et du client

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller;
 
-use App\Service\StripService;
+use App\Repository\ClientRepository;
+use App\Repository\OffreRepository;
+use App\Repository\TransactionRepository;
+use App\Service\StripeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/payment')]
 class PaymentController extends AbstractController
 {
-    #[Route('/payment', name: 'app_payment')]
-    public function index(): Response
+    #[Route('/', name: 'app_payment')]
+    public function index(
+        StripeService $stripeService,
+        OffreRepository $offres,
+        ClientRepository $clients,
+        TransactionRepository $transactions
+        ): Response
     {
+
+        $apiKey = $this->getParameter('STRIPE_API_KEY_SECRET'); // Clé API secrète
+        $offre = $offres->findOneBy(['id' => 25]); // Offre à vendre (titre et montant)
+        $clientEmail = $clients->findOneBy(['id' => 90])->getEmail();
+
+        $stripeService->makePayment(
+            $apiKey,
+            $offre->getMontant(),
+            $offre->getTitre(),
+            $clientEmail
+        );
+
         return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+
+    #[Route('/success', name: 'payment_success')]
+    public function success(): Response
+    {
+        return $this->render('payment/success.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+    #[Route('/cancel', name: 'payment_cancel')]
+    public function cancel(): Response
+    {
+        return $this->render('payment/cancel.html.twig', [
             'controller_name' => 'PaymentController',
         ]);
     }
