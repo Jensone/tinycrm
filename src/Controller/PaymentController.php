e1a79ff10cc3cac9e42ff52b15fe34a1d1818448
Jensone <hello@jensone.com>
2024-02-06 14:14:25 +0100

    🔧 corriger(PaymentController.php) : importer la classe Email de Symfony\Component\Mime 🔧 corriger(PaymentController.php) : importer la classe Transaction de App\Entity 🔧 corriger(PaymentController.php) : importer la classe EntityManagerInterface de Doctrine\ORM 🔧 corriger(PaymentController.php) : importer la classe Request de Symfony\Component\HttpFoundation 🔧 corriger(PaymentController.php) : importer la classe MailerInterface de Symfony\Component\Mailer ✨ fonctionnalité(PaymentController.php) : envoyer un email au client avec le lien de paiement ✨ fonctionnalité(PaymentController.php) : créer une nouvelle transaction et la sauvegarder en base de données

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Form\PaymentType;
+use App\Entity\Transaction;
 use App\Service\StripeService;
+use Symfony\Component\Mime\Email;
 use App\Repository\OffreRepository;
 use App\Repository\ClientRepository;
 use App\Repository\TransactionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/payment')]
 class PaymentController extends AbstractController
@@ -21,27 +25,50 @@ class PaymentController extends AbstractController
         StripeService $stripeService,
         OffreRepository $offres,
         ClientRepository $clients,
-        TransactionRepository $transactions
-        ): Response
-    {
+        MailerInterface $mailer,
+        EntityManagerInterface $em,
+    ): Response {
         $form = $this->createForm(PaymentType::class);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
             $offre = $offres->findOneBy(['id' => $data['offre']->getId()]); // Offre à vendre (titre et montant)
-            $clientEmail = $clients->findOneBy(['id' => $data['client']->getId()])->getEmail();
+            $client = $clients->findOneBy(['id' => $data['client']->getId()]);
             $apiKey = $this->getParameter('STRIPE_API_KEY_SECRET'); // Clé API secrète
+            
             $link = $stripeService->makePayment(
                 $apiKey,
                 $offre->getMontant(),
                 $offre->getTitre(),
-                $clientEmail
+                $client->getEmail()
             );
-            // Envoie du lien au client
+
+            $email = (new Email())
+                ->from('hello@tinycrm.app')
+                ->to($client->getEmail())
+                ->priority(Email::PRIORITY_HIGH)
+                ->subject('Merci de procéder au paiement de votre offre')
+                ->html('<div style="background-color: #f4f4f4; padding: 20px; text-align: center; font-family: sans-serif;">
+                        <h1>Bonjour '.$client->getNomComplet().'</h1><br><br>
+                        <p>Voici le lien pour effectuer le règlement de votre offre :</p><br>
+                        <a href="'.$link.'" target="_blank">Payer</a><br>
+                        <hr>
+                        <p>Ce lien est valable pour une durée limitée.</p><br></div>
+                    ');
+            $mailer->send($email);
+            
+            // Flash message
+
+            $transaction = new Transaction();
+            $transaction->setClient($data['client'])
+                        ->setMontant($offre->getMontant())
+                        ->setStatut('En attente');
+            $em->persist($transaction); // EntityMangerInterface
+            $em->flush();
         }
 
         return $this->render('payment/index.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form,
         ]);
     }
 
@@ -52,6 +79,7 @@ class PaymentController extends AbstractController
             'controller_name' => 'PaymentController',
         ]);
     }
+    
     #[Route('/cancel', name: 'payment_cancel')]
     public function cancel(): Response
     {
