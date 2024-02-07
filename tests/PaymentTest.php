6462984e449835578cfe731a606a84b0e655688a
Jensone <hello@jensone.com>
2024-02-07 12:02:21 +0100

    🐛 corriger(PaymentTest.php) : ajouter un "/" final à l'URL de la requête GET dans le testPaymentRouteWhenLoggedIn() pour correspondre à la route réelle ✨ fonctionnalité(PaymentTest.php) : ajouter une instruction pour couper le kernel s'il est déjà démarré avant de créer le client HTTP

--- tests/PaymentTest.php
+++ tests/PaymentTest.php
@@ -12,17 +12,18 @@ class PaymentTest extends WebTestCase
     /**
      * Ce test nous montre comment mettre en place la vérification du statut de la réponse
      * suite à une requête HTTP. Ici, on vérifie que le statut de la réponse est bien 200.
-     * Cependant, cette étant protégée par un firewall, on doit se connecter avant de se
-     * rendre sur cette page. Pour cela, on utilise fait appel au repository concerné
+     * Cependant, cette route étant protégée par un firewall, on doit se connecter avant
+     * de se rendre sur cette page. Pour cela, on utilise fait appel au repository user
      * puis on le connecte avec la méthode loginUser() du client HTTP de WebTestCase
      */
     public function testPaymentRouteWhenLoggedIn(): void
     {
+        self::ensureKernelShutdown(); // On coupe le kernel s'il est déjà démarré
+        $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
         $adminUser = $userRepository->findOneBy(['email' => 'admin@admin.fr']);
-        $client = static::createClient();
         $client->loginUser($adminUser);
-        $client->request('GET', '/payment');
+        $client->request('GET', '/payment/'); // Pensez au "/" final si pas de paramètres dans l'URL
         $this->assertResponseStatusCodeSame(200);
     }
 }
