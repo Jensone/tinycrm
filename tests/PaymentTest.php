dd6637605a58652abc5be424360a26b05c5281aa
Jensone <hello@jensone.com>
2024-02-07 11:47:46 +0100

    🆕 ajout(PaymentTest.php) : ajouter un test pour vérifier le statut de la réponse lorsqu'un utilisateur est connecté 📝 docs(PaymentTest.php) : ajouter une explication sur la nécessité de se connecter avant d'accéder à la page protégée

---
+++ tests/PaymentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\ClientRepository;
+use App\Repository\OffreRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PaymentTest extends WebTestCase
+{
+    /**
+     * Ce test nous montre comment mettre en place la vérification du statut de la réponse
+     * suite à une requête HTTP. Ici, on vérifie que le statut de la réponse est bien 200.
+     * Cependant, cette étant protégée par un firewall, on doit se connecter avant de se
+     * rendre sur cette page. Pour cela, on utilise fait appel au repository concerné
+     * puis on le connecte avec la méthode loginUser() du client HTTP de WebTestCase
+     */
+    public function testPaymentRouteWhenLoggedIn(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $adminUser = $userRepository->findOneBy(['email' => 'admin@admin.fr']);
+        $client = static::createClient();
+        $client->loginUser($adminUser);
+        $client->request('GET', '/payment');
+        $this->assertResponseStatusCodeSame(200);
+    }
+}
